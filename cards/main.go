266b9e45061fadd45307a38474578dbbe64a4a08
WSixx <lucas.2007s@gmail.com>
2021-04-14 15:45:03 -0300

    FromFile

--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card string = "Ace of Spades"
 	//fmt.Println(card)
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my cards")
 
 }
